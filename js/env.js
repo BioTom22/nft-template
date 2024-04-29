81a98fa16af045884a997a898dcb8536c3d36a4d
BioTom22 <168464129+BioTom22@users.noreply.github.com>
2024-04-29 18:45:46 +0200

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,5 +1,5 @@
-const contractAddress = ""; // Paste the address of the NFT collection.
+const contractAddress = "0xe4311fd7eDD1e05F12D39E11dA8737d65545959A"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
-// const chainId = "0x504"; // Moonbeam
+//const chainId = "0x507"; // Moonbase
+ const chainId = "0x504"; // Moonbeam
 // const chainId = "0x250"; // Astar
